78b9de60133e6c8548a15718627793af1e8433ec
wayne <carzy70203@gmail.com>
2023-04-14 13:51:25 +0800

    試做switch

--- flow/index.php
+++ flow/index.php
@@ -24,4 +24,52 @@ else{
     echo "<br>";
     echo "請繼續努力";
 }
+echo "<br>";
+$level="A";
+switch($level){
+    case "A":
+        echo "表現優良請繼續保持";
+    break;
+    case "B":
+        echo "值得肯定請繼續進步";
+    break;
+    case "C":
+        echo "需要加強";
+    break;
+    case "D":
+        echo "需要多練習加油";
+    break;
+    default: 
+        echo "是否放棄努力?";
+}
+//test
+$goodluck="A";
+switch($goodluck){
+    case "A":
+        echo "天使眷顧著你~凡人!";
+    break;
+    case "B":
+        echo "運氣很好的凡人~";
+    break;
+    case "C":
+        echo "凡人的運氣...";
+    break;
+    case "D":
+        echo "運氣不好的凡人......";
+    break;
+    case "E":
+        echo "可憐的凡人!!!!";
+    break;
+    default: 
+        echo "地獄倒楣鬼,是否運氣太差了?";
+}
+
+
+
+
+
+
+
+
+
 ?>
