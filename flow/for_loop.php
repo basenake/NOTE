937e2333e75a9a91419288dff233fd0f88e438ec
wayne <carzy70203@gmail.com>
2023-04-14 16:35:00 +0800

    for loop 練習

---
+++ flow/for_loop.php
@@ -0,0 +1,43 @@
+<?php
+// for loop
+for($a=1 ; $a<30; $a++){
+    echo $a-10;
+    echo ",";
+}
+$n=100;
+$limit=ceil($n/1);
+echo '$a='.$a;
+echo "<hr>";
+for($a=1 ; $a<$n; $a = $a+2){
+    echo $a;
+    echo ",";
+}
+for($a=2 ; $a<$limit ; $a++){
+    echo $a*2-1;
+    echo ",";
+}
+echo '$a='.$a;
+echo "<hr>";
+for($a=1 ; $a<48; $a++){
+    echo '$a=' .$a."=>";
+    echo $a+$a+1; 
+    echo "<br>";
+}
+echo '$a='.$a;
+echo "<hr>";
+for($a=2 ; $a<97 ; $a++){
+    echo $a*2-1;
+    echo ",";
+}
+
+
+//99乘法表
+for ($x=1 ; $x<=9 ; $x++){
+for ($y=1 ; $y<=9 ; $y++){
+    echo "{$x}*{$y}=" .($x)*($y)  ;
+}
+    echo "<br>";
+}
+
+?>
+
