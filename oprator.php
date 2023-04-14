9ede4fa6d32c14fe62d173a218ddcbc1aee9ec82
wayne <carzy70203@gmail.com>
2023-04-14 10:29:48 +0800

    基本運算

---
+++ oprator.php
@@ -0,0 +1,34 @@
+<?php
+$check=false;
+
+echo $check;
+!$check=false;
+
+echo $check;
+
+echo '<hr>';
+$price=125;
+echo $price;
+$price++;
+echo $price;
+$price--;
+echo $price;
+echo ++$price;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
