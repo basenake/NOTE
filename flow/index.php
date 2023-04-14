cc21ce613791a25a272f81d1eecd2124df229f17
wayne <carzy70203@gmail.com>
2023-04-14 13:37:26 +0800

    簡單的 if else 試算

---
+++ flow/index.php
@@ -0,0 +1,27 @@
+<?php
+
+//if(條件式){真的結果}else{//假的結果 else 不一定要寫}
+
+$score=70;
+if ($score>60){
+    echo "成績=".$score;
+    echo "<br>";
+    echo "恭喜及格了~";}
+else{
+    echo "成績=".$score;
+    echo "<br>";
+    echo "請繼續努力";
+}
+echo "<br>";
+
+$score=50;
+if ($score>60){
+    echo "成績=".$score;
+    echo "<br>";
+    echo "恭喜及格了~";}
+else{
+    echo "成績=".$score;
+    echo "<br>";
+    echo "請繼續努力";
+}
+?>
