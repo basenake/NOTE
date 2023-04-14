4f844c7ffa08cf3f7d7b944a30763eb8b5ef663d
wayne <carzy70203@gmail.com>
2023-04-14 09:45:28 +0800

    note php day one

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+$name="wayne";
+$age=30;
+$total=10*100;
+$cookie=35987*779;
+echo "<span style='color:blue;font-soez:24px'>";
+echo '名子是:';
+echo $name;
+echo '</span>';
+echo '<br>';
+echo "名子是:";
+echo $name;
+echo '<br>';
+echo "<span style='color:red;font-soez:24px'>";
+print ("總量:");
+print ($total);
+echo '<br>';
+echo '</span>';
+echo "<span style='color:black;font-soez:24px'>";
+print ('年齡:');
+print ($age);
+echo '</span>';
+echo '<br>';
+echo "cookie:";
+echo $cookie;
+echo '<br>';
+
+define ("PI",3.14159);
+echo PI;
+echo '<br>';
+
+echo 10*10*PI ;
+echo '<br>';
+
+?>
