0b0c5afb26463ae6af9081a3db248d6444d2c737
wayne <carzy70203@gmail.com>
2023-04-20 08:19:00 +0800

    test for loop

---
+++ flow/test.php
@@ -0,0 +1,32 @@
+<?php
+
+// for($a=0 ; $a<5; $a**){
+//     for($b=0 ; $b<$a; $b++){
+//         echo '*';
+//         echo "<br>";
+//     }
+// }
+echo "<hr>";
+
+
+for($c=0 ; $c<10;$c++){
+    echo "$c";
+    echo "<br>";
+    for ($i=0; $i<=$c; $i++) { 
+    echo "$i";
+
+    }
+
+};
+echo "<hr>";
+// for ($a=1;$a<20 ; $a=$a--) { 
+//     echo "*";
+//     echo "<br>";
+//     for()
+//     echo "*";  
+
+// }
+
+
+
+ ?>
