d1172f99bb9d0607c0e5a13c0ddb3e49f02f9687
wayne <carzy70203@gmail.com>
2023-04-21 08:49:35 +0800

    array test

--- array/array.php
+++ array/array.php
@@ -2,21 +2,39 @@
 
 
 $a[1]="A";
-$a[2]="B";
-$a[3]="C";
-$a[4]="D";
+$b[2]="B";
+$c[3]="C";
+$d[4]="D";
 
-$c=["A","B","C"];
+$e=["A","B","C"];
 
 
 echo $a[1];
 echo "<br>";
-echo $b
+echo $b[2];
+echo "<br>";
+echo $c[3];
+echo "<br>";
+echo $d[4];
+echo "<br>";
+//c2 未被宣告
+//echo $c[2];
+echo "<br>";
+//test for array
+//陣列key  0   1   2   3   4   5   6   7   8   9
+$f=["1","2","3","4","5","6","7","8","9","10"];
 
 
+echo $f[5];
+echo "<br>";
+echo $f[8];
+echo "<br>";
+echo $f[9];
+echo "<br>";
 
 
-
+//$f 沒有 10
+//echo $f[10];
 
 
 
