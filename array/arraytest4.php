0d80a939ee2e2cc36a1c910892eaefcaed8919ee
wayne <carzy70203@gmail.com>
2023-04-21 16:11:28 +0800

    簡單的反轉陣列 不產生新的陣列

--- array/arraytest4.php
+++ array/arraytest4.php
@@ -12,24 +12,53 @@ $land=["子","丑","寅","卯","辰","巳","午","未","申","酉","戌","亥"];
 // 餘數% 
 
 $skyland=[];
-for ($i=0; $i < 60; $i++) { 
-    $skyland= $sky[$i%10] . $land[$i%12];
+for($i=0; $i<60;$i++){ 
+    $skyland=$sky[$i%10] . $land[$i%12];
 }
-
-
 // echo "<pre>";
 // print_r($skyland);
 // echo "</pre>";
-
 echo "<hr>";
+
+
+$skyland=[];
+for($i=0;$i<60;$i++){
+    $skyland[]=$sky[$i%10] . $land[$i%12];
+}
 $year=2053;
-$staryer=1024;
-echo "給定西元年" .$year;
-echo "<br>天干地支為";
+$startYear=1024;  //甲子年
+echo "給定西元年".$year;
+$index=($year-$startYear)%60;
+echo "<br>天干地支年為".$skyland[$index];
+echo "<hr>";
+
+$a=[];
+
+
+echo "<h1>反轉陣列 不產生新的</h1>";
+
+echo '給定陣列$a=[2,4,6,1,8]反轉後 $a=[8,1,6,4,2]';
+echo "原陣列";
+$a=[2,4,6,1,8];
+echo "<pre>";
+echo "原陣列";
+print_r($a);
+echo "</pre>";
+$times=ceil(count($a)/2);
+$maxIndex=count($a)-1;
+
+for ($i=0; $i <$times ; $i++) { 
+    $tmp=$a[$i];
+    $a[$i]=$a[$maxIndex-$i];
+    $a[$maxIndex-$i]=$tmp;
+}
 
 
 
-
+echo "<pre>";
+echo "反轉陣列";
+print_r($a);
+echo "</pre>";
 
 
 
