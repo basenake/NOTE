59a7129330060b2b7d0ef900736658e80d40065d
wayne <carzy70203@gmail.com>
2023-04-24 08:36:25 +0800

    test for chinese

--- string/index.php
+++ string/index.php
@@ -1,15 +1,16 @@
 <?php
+// 一個中文字 位元= 1+2=>3 所以 6個字=3*6=18
 $str ="今天天氣真爛";
-$str ="today is bad day";
+$str2 ="today is bad day";
 echo $str;
 echo "<br>";
 echo $str2;
 echo "<br>";
 echo $str;
 echo "<br>";
-echo substr($str,1,2);
+echo substr($str,0,18);
 echo "<br>";
-echo substr($str,1,2);
+echo substr($str,0,18);
 
 
 
