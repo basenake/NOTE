045509584c3b17336f0eba736b5d8113780e1a5f
wayne <carzy70203@gmail.com>
2023-04-24 08:33:42 +0800

    string

---
+++ string/index.php
@@ -0,0 +1,20 @@
+<?php
+$str ="今天天氣真爛";
+$str ="today is bad day";
+echo $str;
+echo "<br>";
+echo $str2;
+echo "<br>";
+echo $str;
+echo "<br>";
+echo substr($str,1,2);
+echo "<br>";
+echo substr($str,1,2);
+
+
+
+
+
+
+?>
+
