0c0c9a884b805c135b6772de15e9984c2f55db34
wayne <carzy70203@gmail.com>
2023-04-24 13:54:41 +0800

    簡單計算時間

--- date/test.php
+++ date/test.php
@@ -5,17 +5,18 @@
 date_default_timezone_set("Asia/Taipei") ;
 echo strtotime("now");
 $today=strtotime("2023-04-24");
+$today=strtotime(date("Y-m-d"));
 $brithday=strtotime("2023-10-20");
 $gap=($brithday-$today)/(24*60*60);
 echo "<br>";
 echo  "距離" . date("Y-m-d",$brithday) . "還有" .$gap ."天";
 echo "<br>";
-echo "距離明天生日";
+echo "距離明年生日";
 $nextbrithday=strtotime("2024-10-20");
 $gap2=($nextbrithday-$today)/(24*60*60);
 echo  "距離" . date("Y-m-d",$nextbrithday) . "還有" .$gap2 ."天";
-
-
+echo "<br>";
+$brithday2=strtotime("2023-10-07");
 
 
 
