415be40733014a5506cb612636ac094ddd558d3e
wayne <carzy70203@gmail.com>
2023-04-25 15:21:06 +0800

    test for calendar homework

---
+++ date/hw16.php
@@ -0,0 +1,57 @@
+<style>
+    table{
+        border-collapse: collapse;
+    }
+    td{
+        border: 0px ;
+        padding: 10px 15px;
+    }
+</style>
+<h1>4月</h1>
+<?php
+$today=strtotime("now");
+$month=strtotime("now");
+$frd=strtotime("now");
+//firstday
+$fnd=strtotime("now");
+//fianlday
+$frdw=strtotime("now");
+//firstweek
+$fndw=strtotime("now");
+//fianlweek
+$weeks=strtotime("now");
+$frws=strtotime("now");
+//fristweekspace
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
