6575b30d7cdaf0c52758c7f311bad7b345fefd0a
wayne <carzy70203@gmail.com>
2023-04-27 09:24:05 +0800

    days

--- date/calendar/one_years.php
+++ date/calendar/one_years.php
@@ -17,21 +17,45 @@ $finalDateWeek=date("w",strtotime($finalDate));
 $weeks=ceil(($days+$firstDateWeek)/7);
 //除了第一個禮拜 還剩下幾個禮拜
 $firstWeekSpace=$firstDateWeek-1;
+//上個月月曆上顯示的最後一天
+$lastday=date("d",strtotime("-1 month -1 day",$today));
+// 上個月最後一天
+$lastmonthday=date("t",strtotime("-1 month",$today));
+//下個月第一天
+$nextdate=date("j",strtotime("+4 day",$today));
+// 上個月剩下幾天
+$lastmonthdays=$lastmonthday-$lastday;
+
+
+echo "<hr>";
+echo $lastday;
+echo "<hr>";
+echo $nextdate;
+echo "<hr>";
+echo $lastmonthdays;
+echo "<hr>";
 
 
 
+$days=0;
+$week=0;
+$cell=0;
 
-
-
-
-
-
-
-
-
-
-
-
+// echo "<table>";
+// for($i=0;$i<42;$i++){
+//     echo "<tr>";
+//     for($j=0;$j<7;$j++){
+//         for ($k=0;$k<6;$k++){
+//             if($day){
+//                 echo $lastdate++; 
+//             // }else if(){
+//             // }else(){
+//             }
+//         }
+//     }
+//     echo "<tr>";
+// }
+// echo "<table>";
 
 
 
--- date/hw16_01.php
+++ date/hw16_01.php
@@ -37,7 +37,8 @@ for($i=0;$i<$weeks;$i++){
     echo "<tr>";
     for($j=0;$j<7;$j++){
         echo "<td>";
-        if($i==0 and $j<$frdw or $i==0 and $j==$frd || $i==$weeks-1 and $j>$fndw  or $i==$weeks-1 and $j>$fndw){
+        if(($i==0 && $j<$frdw)||(($i==$weeks-1)&&$j>$fndw)){
+        // if($i==0 and $j<$frdw or $i==0 and $j==$frd || $i==$weeks-1 and $j>$fndw  or $i==$weeks-1 and $j>$fndw){
             echo "&nbsp;";
         }else{
             echo $j+7*$i-$frws;
@@ -49,6 +50,42 @@ for($i=0;$i<$weeks;$i++){
 echo "</table>";
 
 
+// 使用陣列表示
+$days=[];
+
+for($i=0;$i<$weeks;$i++){
+    for($j=0;$j<7;$j++){
+        if(($i==0 && $j<$frdw)||(($i==$weeks-1)&&$j>$fndw)){
+            echo "&nbsp;";
+        }else{
+            $days[]=$j+7*$i-$frws;
+        }
+    }
+}
+
+echo "<pre>";
+print_r($days);
+echo "</pre>";
+echo "<hr>";
+echo "<table>";
+echo "<tr>";
+echo "<td>日</td>";
+echo "<td>一</td>";
+echo "<td>二</td>";
+echo "<td>三</td>";
+echo "<td>四</td>";
+echo "<td>五</td>";
+echo "<td>六</td>";
+echo "</tr>";
+for($i=0;$i<count($days)/7;$i++){
+    echo ($i%7==0)?"<tt>":'';
+    echo "<td>";
+    echo $days[$i];
+    echo "</td>";
+    echo ($i%7==6)?"</tr>":'';
+}
+
+echo "</table>";
 
 
 
@@ -57,7 +94,9 @@ echo "</table>";
 
 
 
-
+?>
+<h1>div</h1>
+<?php
 
 
 
